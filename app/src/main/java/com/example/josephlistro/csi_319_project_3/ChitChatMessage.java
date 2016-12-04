bed0e906bbe1bb6dcb9cab1c01f3c3401c86789d
Joe L <jlistro@gmail.com>
2016-12-04 15:56:06 -0500

    ChitChatMessage Object Built

--- app/src/main/java/com/example/josephlistro/csi_319_project_3/ChitChatMessage.java
+++ app/src/main/java/com/example/josephlistro/csi_319_project_3/ChitChatMessage.java
@@ -6,7 +6,31 @@ package com.example.josephlistro.csi_319_project_3;
 
 public class ChitChatMessage {
     // Container for messages
-    private String mSubject;
-    private String mId;
+    private String mMessage;
+    private String mClient;
     private String mUrl;
+
+    public String getMessage() {
+        return mMessage;
+    }
+
+    public String getClient() {
+        return mClient;
+    }
+
+    public void setMessage(String message) {
+        mMessage = message;
+    }
+
+    public void setClient(String client) {
+        mClient = client;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
 }

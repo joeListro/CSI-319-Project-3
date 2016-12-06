6d426801909197fda665c50512743d8f31058363
Alexander Bean Apmann <caboose200@gmail.com>
2016-12-06 06:03:43 -0500

    killed all the things

--- app/src/main/java/com/example/josephlistro/csi_319_project_3/ChitChatFragment.java
+++
@@ -1,7 +0,0 @@
-package com.example.josephlistro.csi_319_project_3;
-
-public class ChitChatFragment extends VisibleFragment {
-    public static ChitChatFragment newInstance() {
-        return new ChitChatFragment();
-    }
-}

ed135f51633aa92f0e243613fa9c12849a6bf527
Joe Listro <jlistro@gmail.com>
2016-12-06 10:38:19 -0500

    Added HeckingShout value object.

--- app/src/main/java/com/upmoon/alex/moonyodel/HeckingShout.java
+++ app/src/main/java/com/upmoon/alex/moonyodel/HeckingShout.java
@@ -5,4 +5,73 @@ package com.upmoon.alex.moonyodel;
  */
 
 public class HeckingShout {
+    private String mMessageID;
+    private String mTimestamp;
+    private String mMessageContent;
+    private int mLikes;
+    private int mDislikes;
+
+    public HeckingShout(String id, String time, String content, int likes, int dislikes)
+    {
+        mMessageID = id;
+        mTimestamp = time;
+        mMessageContent = content;
+        mLikes = likes;
+        mDislikes = dislikes;
+    }
+
+    public String getMessageID()
+    {
+        return mMessageID;
+    }
+
+    public void setMessageID(String id) {
+        mMessageID = id;
+    }
+
+    public String getMessageTimestamp()
+    {
+        return mTimestamp;
+    }
+
+    public void setMessageTimestamp(String time)
+    {
+        mTimestamp = time;
+    }
+
+    public String getMessageContent()
+    {
+        return mMessageContent;
+    }
+
+    public void setMessageContent(String content)
+    {
+        mMessageContent = content;
+    }
+
+    public int getLikes()
+    {
+        return mLikes;
+    }
+
+    public void setLikes(int likes)
+    {
+        mLikes = likes;
+    }
+
+    public int getDislikes()
+    {
+        return mDislikes;
+    }
+
+    public void setDislikes(int dislikes)
+    {
+        mDislikes = dislikes;
+    }
+
+    @Override
+    public String toString()
+    {
+        return mMessageContent;
+    }
 }
